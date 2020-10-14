96b3d0799b76df81932499cd12385da8b35f4fec
Wook-2 <bwson98@gmail.com>
2020-10-14 15:30:29 +0900

    add exam code 10/14

--- example_code/defer.go
+++ example_code/defer.go
@@ -2,18 +2,17 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	a := 10
 	test_defer(&a)
 	fmt.Println(a)
 }
 
-func add(i **int){
-	**i ++
+func add(i **int) {
+	**i++
 }
 
-func test_defer(a *int){
-	//defer add(&a)
-	defer *a = 15
-	fmt.Println("defer:",*a)
+func test_defer(a *int) {
+	defer add(&a)
+	fmt.Println("defer:", *a)
 }
---
+++ example_code/pkg.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	m "./math"
+)
+
+func main() {
+	xs := []float64{1, 2, 3, 4}
+	avg := m.Average(xs)
+	fmt.Println(avg)
+}
